2a257461de0d00f171e9f39d683b0c3d561fbe1a
danang <danangekoyudanto1995@gmail.com>
2020-02-08 20:33:10 +0700

    set up apollo middleware and afterware

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,54 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql'
+import { store } from './redux';
+import { meActions } from './redux/reducers/me';
+
+const httpLink = createHttpLink({ uri: 'http://localhost:3000/graphql' });
+
+const middlewareLink = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  const { me } = store.getState();
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      'x-token': `Admin ${me.access_token}`,
+      'x-refresh-token': `Admin ${me.refresh_token}`,
+      'x-client-id': me.client_id,
+      'x-provider': me.provider
+    }
+  }));
+
+  return forward(operation);
 });
 
-export default client;
+const afterwareLink = new ApolloLink((operation, forward) =>
+  forward(operation).map(response => {
+    const {
+      response: { headers }
+    } = operation.getContext();
+    if (headers) {
+      const token = headers.get('x-token');
+      const refreshToken = headers.get('x-refresh-token');
+
+      if (token) {
+        store.dispatch(meActions.customSet('access_token', token));
+      }
+
+      if (refreshToken) {
+        store.dispatch(meActions.customSet('refresh_token', refreshToken));
+      }
+    }
+
+    return response;
+  })
+);
+
+const httpLinkWithMiddleware = afterwareLink.concat(middlewareLink.concat(httpLink));
+
+export default new ApolloClient({
+  link: httpLinkWithMiddleware,
+  cache: new InMemoryCache()
+});
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import gql from 'graphql-tag';
+import { useLazyQuery } from '@apollo/react-hooks';
+
+const GET_ADMINS = gql`
+  {
+    admins {
+      id
+      full_name
+      email
+      phone
+    }
+  }
+`;
 
 function Home() {
-  return <div>Home</div>;
+  const [getAdmins, { loading, data, error }] = useLazyQuery(GET_ADMINS);
+
+  useEffect(() => {
+    if (error) {
+      error.graphQLErrors.map(({ message }, i) => {
+        console.log(message);
+      });
+    }
+
+    if (data) {
+      console.log(data);
+    }
+  }, [error, data]);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: 625,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <button
+        style={{
+          height: 45,
+          width: 250,
+          borderStyle: 'solid',
+          border: 'none',
+          borderRadius: 4,
+          color: 'white',
+          fontSize: 16,
+          backgroundColor: '#498de6',
+          cursor: 'pointer'
+        }}
+        onClick={() => {
+          getAdmins();
+        }}
+      >
+        GET ADMINS
+      </button>
+    </div>
+  );
 }
 
 export default Home;
